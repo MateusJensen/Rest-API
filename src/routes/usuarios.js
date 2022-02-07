a29ef160ad1cb3799f336bf7df093f68590ab11c
MateusJensen <mateusjensen05@gmail.com>
2022-02-07 16:03:01 -0300

    Login
    Adicionada rota de login

--- src/routes/usuarios.js
+++ src/routes/usuarios.js
@@ -15,7 +15,7 @@ router.post('/cadastro', (req, res, next) => {
         conn.query(
             'SELECT * FROM usuarios WHERE email = ?;',
             [req.body.email],
-            (error, resultado, field) =>{
+            (error, resultado, field) => {
 
                 if (error) {
                     return res.status(500).send({
@@ -23,7 +23,7 @@ router.post('/cadastro', (req, res, next) => {
                     })
                 }
 
-                if (resultado.length > 0){
+                if (resultado.length > 0) {
                     return res.status(409).send({
                         mensagem: 'O email informado ja se encontra cadastrado!'
                     });
@@ -35,18 +35,18 @@ router.post('/cadastro', (req, res, next) => {
                             error: errBcrypt
                         })
                     }
-        
+
                     conn.query(`INSERT INTO usuarios (email, senha) VALUES (?, ?)`,
                         [req.body.email, hash],
                         (error, resultado, field) => {
                             conn.release();
-        
+
                             if (error) {
                                 return res.status(500).send({
                                     error: error
                                 })
                             }
-        
+
                             const response = {
                                 mensagem: 'Usuario criado com sucesso!',
                                 usuario: {
@@ -60,6 +60,55 @@ router.post('/cadastro', (req, res, next) => {
                 });
             }
         )
+    });
+});
+
+router.post('/login', (req, res, next) => {
+    mysql.getConnection((error, conn) => {
+
+        if (error) {
+            return res.status(500).send({
+                error: error
+            })
+        }
+
+        const query = 'SELECT * FROM usuarios WHERE email = ?;'
+        const params = [req.body.email];
+
+        conn.query(query, params, (error, resultado) => {
+            conn.release();
+
+            if (error) {
+                return res.status(500).send({
+                    error: error
+                })
+            }
+
+            if (resultado.length < 1) {
+                return res.status(401).send({
+                    mensagem: 'Falha na autenticação'
+                });
+            }
+
+            bcrypt.compare(req.body.senha, resultado[0].senha, (err, result) => {
+
+                if (err) {
+                    return res.status(401).send({
+                        mensagem: 'Falha na autenticação'
+                    });
+                }
+
+                if (result) {
+                    return res.status(200).send({
+                        mensagem: 'Autenticado com sucesso'
+                    });
+                }
+
+                return res.status(401).send({
+                    mensagem: 'Falha na autenticação'
+                });
+            });
+        });
     });
 });
 
